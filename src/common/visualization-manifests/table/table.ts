6a33bc31c65a66ec7a46602f5e711cc457044edc
kit <karamba66@ukr.net>
2025-12-24 16:51:43 +0100

    increase prio for automatic selection

--- src/common/visualization-manifests/table/table.ts
+++ src/common/visualization-manifests/table/table.ts
@@ -43,7 +43,7 @@ const rulesEvaluator = visualizationDependentEvaluatorBuilder
 
     const changed = !newSplits.equals(splits);
     return changed
-      ? Resolve.automatic(6, { splits: newSplits })
+      ? Resolve.automatic(7, { splits: newSplits })
       : Resolve.ready(isSelectedVisualization ? 10 : 7);
   })
   .build();
